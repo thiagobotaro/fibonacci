f1e775ddbdb9efedb85a3d4d628c92d584bcd079
thiagobotaro <thiagobotaro@hotmail.com>
2024-02-29 15:07:59 -0300

    Patch 1

---
+++ app.js
@@ -0,0 +1,28 @@
+let tabela = [0, 1];
+let numero = 0;
+
+
+function adicionar(){
+numero = parseInt(document.getElementById('campo-1').value);
+let contador = 0;
+console.log('numero:' + numero);
+
+let resultado = document.getElementById('resultado');
+
+while(contador < numero){
+let ultimo = tabela.length - 1;
+let penultimo = tabela.length - 2;
+let novo = tabela[ultimo] + tabela[penultimo];
+tabela.push(novo);
+resultado.innerHTML = tabela;
+contador++;
+}
+}
+
+function limpar(){
+    console.clear();
+    tabela = [0, 1];
+    numero = '0';
+    resultado.innerHTML = '';
+    document.getElementById('campo-1').value = '';
+}
